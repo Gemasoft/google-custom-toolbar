251d976eb767207d0d3225c3dba8a7c2c2aafd8c
gerardo.martinez@sanmina.com <gemasoft.games@gmail.com>
2019-08-29 14:08:21 -0500

    -Fixed for last google apps versions -Icons changed

--- actions.js
+++ actions.js
@@ -6,9 +6,6 @@ $(document).ready(function () {
 function reload() {
     //Remove all the current icons and then proceed loading them again
     $(".customButton").remove();
-    //Get the toolbar element
-    var div = $(".gb_ib")[0];
-    var buttons = "";
     //Get the current settings saved
     chrome.storage.sync.get({
         calendar: true,
@@ -18,26 +15,33 @@ function reload() {
     }, function (items) {
         //Enable/Disable buttons based on the saved settings
         if (items.calendar) {
-            var btn = $(".gb_Z:has(a:has(span:contains('Calendar')))");
-            if (btn[0] != undefined)
-                div.prepend(btn.clone().addClass("customButton")[0]);
+            appendButton("Calendar", "https://calendar.google.com/calendar/r", "https://www.gstatic.com/images/branding/product/1x/calendar_48dp.png")
         }
         if (items.sheets) {
-            var btn = $(".gb_Z:has(a:has(span:contains('Hojas'), :contains('Spread'), :contains('Sheets')))");
-            if (btn[0] != undefined)
-                div.prepend(btn.clone().addClass("customButton")[0]);
+            appendButton("Sheets", "https://docs.google.com/spreadsheets/u/0/", "https://www.gstatic.com/images/branding/product/1x/sheets_48dp.png")
         }
         if (items.docs) {
-            var btn = $(".gb_Z:has(a:has(span:contains('Doc')))");
-            if (btn[0] != undefined)
-                div.prepend(btn.clone().addClass("customButton")[0]);
+            appendButton("Docs", "https://docs.google.com/document/u/0/", "https://www.gstatic.com/images/branding/product/1x/docs_48dp.png")
         }
         if (items.drive) {
-            var btn = $(".gb_Z:has(a:has(span:contains('Drive')))");
-            if (btn[0] != undefined)
-                div.prepend(btn.clone().addClass("customButton")[0]);
+            appendButton("Drive", "https://drive.google.com/drive/u/0", "https://www.gstatic.com/images/branding/product/1x/drive_48dp.png")
         }
         //Align icons and remove text
-        $(".customButton").css("height", "80px").css("width", "60px").find(".gb_3").remove();
+        //$(".customButton").css("height", "80px").css("width", "60px").find(".gb_3").remove();
     });
+}
+
+function appendButton(buttonDesc, appLink, imgSrc){
+    //Get the toolbar element
+    var div = $(".gb_Zc.gb_4d");
+    //Append the button
+    div.append(
+        $('<div class="gb_we gb_ue customButton">').append(
+            $('<div class="zo" data-tooltip="'+ buttonDesc +'">').append(
+                $('<a class="gb_ze gb_xe gb_tb" href="'+ appLink +'" target="_blank">').append(
+                    $('<img style="opacity: 100;" src="'+ imgSrc +'">')
+                )
+            )  
+        )
+    );
 }
